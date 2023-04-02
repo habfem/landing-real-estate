2d5cc877d87ee9e79454e37107e1c95f6d114709
habfem <habeebmustapha96@gmail.com>
2023-04-02 16:02:53 -0500

    initial commit

--- src/properties.js
+++ src/properties.js
@@ -1,13 +1,13 @@
-import house1 from "./media/image1.png";
-import house2 from "./media/image2.png";
-import house3 from "./media/image3.png";
+import house1 from "./media/image1.jpg";
+import house2 from "./media/image_2.jpeg";
+import house3 from "./media/image3.jpg";
 
 export const properties = [
   {
     id: "1",
     img: house1,
-    price: "35000",
-    address: "8502 Preston Rd. Inglewood, Maine 98280",
+    price: "200,000",
+    address: "215 Awolow way Alausa ikeja",
     bedrooms: 2,
     bathrooms: 2,
     space: 2000,
@@ -16,8 +16,8 @@ export const properties = [
   {
     id: "2",
     img: house2,
-    price: "38000",
-    address: "Wailuku, HI 96793",
+    price: "700,000",
+    address: "969 Main Street",
     bedrooms: 3,
     bathrooms: 2,
     space: 2300,
@@ -26,8 +26,8 @@ export const properties = [
   {
     id: "3",
     img: house3,
-    price: "47000",
-    address: "2917 23rd St",
+    price: "400,000",
+    address: "2917 15th St Ijaiye",
     bedrooms: 4,
     bathrooms: 3,
     space: 3000,
